e22a765f97e9e59278f15692d90f4fd9253eb5db
bcbeckman <bbeckman@gsitechnology.com>
2021-10-19 06:16:18 -0700

    update pkg deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
 (define collection "bleir-sandbox")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("beautiful-racket-lib"
+               "base"))
+(define build-deps '("wxme-lib"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/bleir-sandbox.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
